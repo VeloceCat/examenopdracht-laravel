0db74f2464460edcd6362ac859bb2bf7e7611c6d
VeloceCat <velocecat@gmail.com>
2018-01-21 00:08:10 +0100

    first-day-upload

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+use App\Http\Requests\CreatePostRequest;
+// use App\Http\Requests\UpdatePostRequest;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'desc')->paginate(10);
+
+        return view('posts.index')->with(['posts' => $posts]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(CreatePostRequest $request)
+    {
+        $post = Post::create($request->only('title', 'description', 'url'));
+
+        return redirect()->route('posts_path');
+    }
+
+}
+
